d8e355d6ac6f41793ab45e2a49418a90e4451f43
sanjana gopu <mailsanjana.gopu@gmail.com>
2019-10-19 22:39:49 +0000

    added comment lines for new print statement

--- Hello.java
+++ Hello.java
@@ -4,6 +4,7 @@ public class Hello {
    public static void main(String[] args) {
       // Prints "Hello, World" in the terminal window.
       System.out.println("Hello, World");
+//Prints "Welcome to git" on terminal screen
       System.out.println("Welcome to git");
    }
 }
